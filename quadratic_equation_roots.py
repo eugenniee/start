21806ef7388101d114f9a2e802323699e6155f51
eugenniee <evgenia.shtyrenko@gmail.com>
2015-02-26 23:08:33 +0100

    Remove srqt_d variable

--- quadratic_equation_roots.py
+++ quadratic_equation_roots.py
@@ -6,13 +6,12 @@ def smaller_root (a, b, c):
     d = b ** 2 - 4 * a * c # d - discriminant associated with the given equation
     if d < 0:
         return "Error: No real solutions"
-    sqrt_d = math.sqrt(d)
     if d == 0:
-        x = (-b + sqrt_d) / (2 * a)
+        x = (-b + math.sqrt(d)) / (2 * a)
         return "The only solution is " + str(x)
     else:
-        x1 = (-b + sqrt_d) / (2 * a)
-        x2 = (-b - sqrt_d) / (2 * a)
+        x1 = (-b + math.sqrt(d)) / (2 * a)
+        x2 = (-b - math.sqrt(d)) / (2 * a)
         if (x1 > x2):
             return x2
         else:
